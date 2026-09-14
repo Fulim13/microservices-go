7bb02e974b077fba82d656aef2755dd7d3c31e62
Fu Lim <fulim1130@gmail.com>
2026-09-15 04:37:12 +0800

    feat: add grpc server for order service

---
+++ order/internal/adapters/grpc/grpc.go
@@ -0,0 +1,26 @@
+package grpc
+
+import (
+	"context"
+
+	"github.com/Fulim13/microservices-go/order/internal/application/core/domain"
+	"github.com/Fulim13/microservices-proto/golang/order"
+)
+
+// go get -u github.com/Fulim13/microservices-proto/golang/order
+func (a Adapter) Create(ctx context.Context, request *order.CreateOrderRequest) (*order.CreateOrderResponse, error) {
+	var orderItems []domain.OrderItem
+	for _, orderItem := range request.OrderItems {
+		orderItems = append(orderItems, domain.OrderItem{
+			ProductCode: orderItem.ProductCode,
+			UnitPrice:   orderItem.UnitPrice,
+			Quantity:    orderItem.Quantity,
+		})
+	}
+	newOrder := domain.NewOrder(request.UserId, orderItems)
+	result, err := a.api.PlaceOrder(ctx, newOrder)
+	if err != nil {
+		return nil, err
+	}
+	return &order.CreateOrderResponse{OrderId: result.ID}, nil
+}
