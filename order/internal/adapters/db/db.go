b673986bd4628a610810a41ca703b87ef1ad4cdb
Fu Lim <fulim1130@gmail.com>
2026-09-15 03:49:16 +0800

    feat: add db adapter implementation for order service

---
+++ order/internal/adapters/db/db.go
@@ -0,0 +1,87 @@
+package db
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/Fulim13/microservices-go/order/internal/application/core/domain"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+// go get -u gorm.io/gorm
+// get -u gorm.io/driver/postgre
+type Order struct {
+	gorm.Model
+	CustomerID int64
+	Status     string
+	OrderItems []OrderItem
+}
+
+type OrderItem struct {
+	ProductCode string
+	Unit_Price  float32
+	Quantity    int32
+	OrderID     uint
+}
+
+type Adapter struct {
+	db *gorm.DB
+}
+
+func NewAdapter(dataSourceUrl string) (*Adapter, error) {
+	db, err := gorm.Open(postgres.Open(dataSourceUrl), &gorm.Config{})
+	if err != nil {
+		return nil, fmt.Errorf("db connection error: %v", err)
+	}
+
+	err = db.AutoMigrate(&Order{}, &OrderItem{})
+	if err != nil {
+		return nil, fmt.Errorf("db migration error: %v", err)
+	}
+
+	return &Adapter{db: db}, nil
+}
+
+func (a Adapter) Get(ctx context.Context, id int64) (domain.Order, error) {
+	var orderEntity Order
+	res := a.db.First(&orderEntity, id)
+	var orderItems []domain.OrderItem
+	for _, orderItem := range orderEntity.OrderItems {
+		orderItems = append(orderItems, domain.OrderItem{
+			ProductCode: orderItem.ProductCode,
+			UnitPrice:   orderItem.Unit_Price,
+			Quantity:    orderItem.Quantity,
+		})
+	}
+
+	order := domain.Order{
+		ID:         int64(orderEntity.ID),
+		CustomerID: orderEntity.CustomerID,
+		Status:     orderEntity.Status,
+		OrderItems: orderItems,
+		CreatedAt:  orderEntity.CreatedAt.UnixNano(),
+	}
+	return order, res.Error
+}
+
+func (a Adapter) Save(ctx context.Context, order *domain.Order) error {
+	var orderItems []OrderItem
+	for _, orderItem := range order.OrderItems {
+		orderItems = append(orderItems, OrderItem{
+			ProductCode: orderItem.ProductCode,
+			Unit_Price:  orderItem.UnitPrice,
+			Quantity:    orderItem.Quantity,
+		})
+	}
+	orderModel := Order{
+		CustomerID: order.CustomerID,
+		Status:     order.Status,
+		OrderItems: orderItems,
+	}
+	res := a.db.Create(orderModel)
+	if res.Error == nil {
+		order.ID = int64(orderModel.ID)
+	}
+	return res.Error
+}
