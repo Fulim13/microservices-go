102947b3cce72df47a19435f1c7313d0b938c987
Fu Lim <fulim1130@gmail.com>
2026-09-15 20:44:26 +0800

    feat: add circuit breaker between order and payment service

--- order/internal/adapters/payment/payment.go
+++ order/internal/adapters/payment/payment.go
@@ -2,11 +2,13 @@ package payment
 
 import (
 	"context"
+	"log"
 	"time"
 
 	"github.com/Fulim13/microservices-go/order/internal/application/core/domain"
 	"github.com/Fulim13/microservices-proto/golang/payment"
 	grpc_retry "github.com/grpc-ecosystem/go-grpc-middleware/retry"
+	"github.com/sony/gobreaker"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/credentials/insecure"
@@ -32,12 +34,34 @@ func NewAdapter(paymentServiceUrl string) (*Adapter, error) {
 	return &Adapter{payment: client}, nil
 }
 
+// go get -u github.com/sony/gobreaker
 func (a *Adapter) Charge(order *domain.Order) error {
 	// ctx, _ := context.WithTimeout(context.TODO(), time.Second*3)
-	_, err := a.payment.Create(context.Background(), &payment.CreatePaymentRequest{
-		UserId:     order.CustomerID,
-		OrderId:    order.ID,
-		TotalPrice: order.TotalPrice(),
+	cb := gobreaker.NewCircuitBreaker(gobreaker.Settings{
+		Name:        "cbPayment", // Unique name of circuit breaker
+		MaxRequests: 0,           // Allowed number of requests for half open circult
+		Timeout:     4,           // Timeout for an open to half-open transition
+		ReadyToTrip: func(counts gobreaker.Counts) bool { // Decide if the circuit will be open
+			failureRatio := float64(counts.TotalFailures) / float64(counts.Requests)
+			return failureRatio >= 0.6
+		},
+		OnStateChange: func(name string, from, to gobreaker.State) { // Execute on each state changes
+			log.Printf("Circuit Breaker: %s, changed from %v, to %v", name, from, to)
+		},
 	})
-	return err
+
+	paymentResponse, errCreate := cb.Execute(func() (interface{}, error) {
+		return a.payment.Create(context.Background(), &payment.CreatePaymentRequest{
+			UserId:     order.CustomerID,
+			OrderId:    order.ID,
+			TotalPrice: order.TotalPrice(),
+		})
+	})
+	if errCreate != nil {
+		log.Printf("Failed to create payment. Err: %v", errCreate)
+	} else {
+		log.Printf("Payment %d is created successfully.", paymentResponse.(*payment.CreatePaymentResponse).PaymentId)
+	}
+
+	return errCreate
 }
